0f919bc5004b0bdeb4b0633b87cd4c8145208e01
kouseidayo <kousei-takahashi@mefure.co.jp>
2023-03-31 12:13:38 +0900

    フォロワー削除2

--- SNS/main/models.py
+++ SNS/main/models.py
@@ -3,12 +3,23 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(get_user_model(),verbose_name="ユーザー",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.comment_text)
+    
     
 class Message(models.Model):
     msg_id = models.AutoField(primary_key=True)
     msg_text = models.TextField()
-    good = models.PositiveIntegerField()
+    date_created = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(get_user_model(),verbose_name="ユーザー",on_delete=models.CASCADE)
+    liked_by = models.ManyToManyField(get_user_model(), related_name='liked_posts', blank=True)
+    commented_by = models.ManyToManyField(Comment, related_name='comment', blank=True)
 
     def __str__(self):
         return str(self.msg_text)
--- SNS/main/views.py
+++ SNS/main/views.py
@@ -1,54 +1,87 @@
 from django.shortcuts import render
-from .models import Message
-from django.shortcuts import redirect
+from .models import Message,Comment
+from django.shortcuts import get_object_or_404,redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
-
-
+##if not request.user.is_authenticated: #未ログインはログインページにリダイレクト
+##        return redirect('account_login')
 
 
 def user_create(get_id):
     User = get_user_model()
     user = User.objects.get(id=get_id)
     return user
-    
+
+
+#@login_required    
 def msg_list(request):
-##    if not request.user.is_authenticated: #未ログインはログインページにリダイレクト
-##        return redirect('account_login')
-    
     msg = Message.objects.all()
-    return render(request, 'main/msg_list.html', {'msg':msg})
+    for text in msg:
+        text.likes = text.liked_by.count()
+        if text.liked_by.filter(id=request.user.id).exists():
+            text.liked=True
+        else:
+            text.liked=False
+    
+    return render(request, 'main/msg_list.html', {'msg': msg})
 
 
+@login_required  
 def post_page(request):
-    if not request.user.is_authenticated: #未ログインはログインページにリダイレクト
-        return redirect('account_login')
-    
-    return render(request,'post/post_page.html')
+        return render(request,'post/post_page.html')
 
 
+@login_required  
 def msg_post(request):
-    if not request.user.is_authenticated: #未ログインはログインページにリダイレクト
-        return redirect('account_login')
-    
     Message.objects.create(msg_text=request.POST.get("msg_text"),
-                             good=request.POST.get("good"),
                              user = user_create(request.user.id)
                              )
-
     return redirect('msg_list')
 
-#https://teratail.com/questions/288797
-def my_page(request):
-    if not request.user.is_authenticated: #未ログインはログインページにリダイレクト
-        return redirect('account_login')
 
+@login_required  
+def my_page(request):
     msg = Message.objects.filter(user=request.user)
+    for text in msg:
+        text.likes = text.liked_by.count()
+        
     return render(request, 'main/my_page.html', {'msg':msg})
 
 
+@login_required  
+def get_likes(request,pk):
+    post = get_object_or_404(Message, pk=pk)
+    liked = False
+    if post.liked_by.filter(id=request.user.id).exists():
+        liked = True
+    if request.method == 'POST':
+        if 'like' in request.POST:
+            if liked:
+                post.liked_by.remove(request.user)
+            else:
+                post.liked_by.add(request.user)
+            return redirect('msg_list')
+    return render(request, 'msg_list.html', {'post': post, 'liked': liked})
 
 
-#djangoでgood機能の実装方法を教えて  bing
+@login_required
+def comment_page(request,pk):
+    target = get_object_or_404(Message, pk=pk)
+    comments_list = target.commented_by.all()
+    return render(request, 'comment/comment_page.html', {'target':target,'comments_list':comments_list})
+
+    
+@login_required  
+def get_comments(request,pk):
+    comment = Comment.objects.create(comment_text=request.POST.get("comment_text"),
+                                     user = user_create(request.user.id)
+                                     )
+
+    post = get_object_or_404(Message, pk=pk)
+    post.commented_by.add(comment)
+    return redirect('msg_list')
+
+
 
     
